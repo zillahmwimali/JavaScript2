d1a85768a55b0234eca60fc079ec462e3348d297
zillahmwimali <zillahmwimmali@gmail.com>
2021-03-21 20:32:36 +0300

    Asiignment on Switch Statements

---
+++ assignmenttwo.js
@@ -0,0 +1,26 @@
+function getGrade(marks){
+    var marks=87;
+    if(marks >=80){
+        console.log("A");
+    }
+    else if(marks >=70){
+        console.log("B");
+   
+    }
+    else if(marks>=60){
+       console.log("C");
+   
+    }
+    else if(marks>=50){
+        console.log("D");
+   }
+   else if(marks<=40){
+       console.log("E");
+   }
+   else {
+       console.log("F");
+   }
+   
+   
+   }
+   getGrade()
